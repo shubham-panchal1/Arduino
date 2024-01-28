f48cee0042d4d2f3ac370bd35bf9b2098e64622b
shubham-panchal1 <153976958+shubham-panchal1@users.noreply.github.com>
2024-01-28 02:00:37 -0600

    Create Board_Pin.ino

---
+++ Board_Pin/Board_Pin.ino
@@ -0,0 +1,23 @@
+void setup() {
+ 
+  Serial.begin(115200);
+ 
+  Serial.print("MOSI: ");
+  Serial.println(MOSI);
+  Serial.print("MISO: ");
+  Serial.println(MISO);
+  Serial.print("SCK: ");
+  Serial.println(SCK);
+  Serial.print("SS: ");
+  Serial.println(SS); 
+ 
+  Serial.print("SDA: ");
+  Serial.println(SDA); 
+  Serial.print("SCL: ");
+  Serial.println(SCL); 
+ 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
